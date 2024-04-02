0c78e8a2ccba0f385718cfe4859d4def7234262f
foonerd <andrew@dt-ltd.net>
2024-04-02 15:02:42 +0100

    Remove plymouth theme from global /usr file copy

--- scripts/volumio/configure.sh
+++ scripts/volumio/configure.sh
@@ -88,8 +88,11 @@ cp "${SRC}/volumio/bin/dynswap.sh" "${ROOTFS}/bin/dynswap.sh"
 cp "${SRC}/volumio/bin/rename_netiface0.sh" "${ROOTFS}/bin/rename_netiface0.sh"
 chmod a+x "${ROOTFS}/bin/rename_netiface0.sh"
 
-#Plymouth & upmpdcli files
-cp -rp "${SRC}"/volumio/usr/* "${ROOTFS}/usr/"
+#Lib files
+cp -rp "${SRC}"/volumio/usr/lib/* "${ROOTFS}/usr/lib/"
+
+#Upmpdcli files
+cp -rp "${SRC}"/volumio/usr/share/upmdcli/* "${ROOTFS}/usr/share/upmdcli/"
 
 #CPU TWEAK
 cp "${SRC}/volumio/bin/volumio_cpu_tweak" "${ROOTFS}/bin/volumio_cpu_tweak"
